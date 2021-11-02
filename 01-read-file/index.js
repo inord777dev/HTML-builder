ccac6447877dd2670581761fdec19cbfe3bd0b8f
inord777dev <inord777dev@gmail.com>
2021-11-02 09:22:53 +0300

    init: complete 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const path = require('path');
+const { stdout } = process;
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'));
+
+let data = '';
+stream.on('data', chunk => data += chunk);
+stream.on('end', () => stdout.write(data));
+stream.on('error', error => stdout.write('Error: ' + error.message));
